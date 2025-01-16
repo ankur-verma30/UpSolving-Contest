6f1fbe5872ee9810bf1560a2e14460860d608bcd
Ankur Verma <ankurverma7619@gmail.com>
2025-01-16 17:48:42 +0530

    Starter 169

---
+++ Starter 169/Make_k_Most_frequent.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define MOD 1000000007
+ 
+//Ankur Verma
+int solve(){
+
+}
+
+int main()
+{
+int testcase;
+cin>>testcase;
+for(int i=0;i<testcase;i++){
+int n;
+cin>>n;
+
+cout<<solve()<<endl;
+}
+return 0;
+}

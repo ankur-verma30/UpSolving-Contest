d7cc7dd0cf226b234f1a5190a0614f0e55152c86
Ankur Verma <ankurverma7619@gmail.com>
2025-01-16 19:04:03 +0530

    Upsolved Starter 169

--- Starter 169/Make_k_Most_frequent.cpp
+++ Starter 169/Make_k_Most_frequent.cpp
@@ -1,25 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 #define ll long long
 #define MOD 1000000007
- 
-//Ankur Verma
-int solve(){
 
+int solve(int n, int k, vector<int> &arr)
+{
+    vector<int> mpp(21, 0);
+
+    for (const auto &it : arr)
+        mpp[it]++;
+
+    // If k is already the most frequent element
+    if (*max_element(mpp.begin(), mpp.end()) == mpp[k])
+        return 0;
+
+    // Check from the prefix
+    auto temp = mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]--;
+        if (mpp[k] == 0)
+            break;
+        if (*max_element(mpp.begin(), mpp.end()) == mpp[k])
+            return 1;
+    }
+
+    // check the suffix
+    mpp = temp;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        mpp[arr[i]]--;
+        if (mpp[k] == 0)
+            break;
+        if (*max_element(mpp.begin(), mpp.end()) == mpp[k])
+            return 1;
+    }
+
+    return 2; 
 }
 
 int main()
 {
-int testcase;
-cin>>testcase;
-for(int i=0;i<testcase;i++){
-int n;
-cin>>n;
+    int testcase;
+    cin >> testcase;
+    for (int i = 0; i < testcase; i++)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
 
-cout<<solve()<<endl;
-}
-return 0;
-
-//This is the new code on the file
+        cout << solve(n, k, arr) << endl;
+    }
+    return 0;
 }
